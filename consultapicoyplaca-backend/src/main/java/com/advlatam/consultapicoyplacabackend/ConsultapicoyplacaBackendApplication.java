42a3fdb46b4ae1a846d9884686ae51a5e89d5ce3
Wilman-Armijos-1604 <w.armijos_live@hotmail.com>
2023-08-02 11:31:14 -0500

    Creacion y Configuracion Inicial de Angular y SpringBoot

---
+++ consultapicoyplaca-backend/src/main/java/com/advlatam/consultapicoyplacabackend/ConsultapicoyplacaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.advlatam.consultapicoyplacabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultapicoyplacaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultapicoyplacaBackendApplication.class, args);
+	}
+
+}
---
+++ consultapicoyplaca-backend/src/test/java/com/advlatam/consultapicoyplacabackend/ConsultapicoyplacaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advlatam.consultapicoyplacabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultapicoyplacaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
